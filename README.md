5bb90894c9f7e556440d28c714adbaf684123c19
이승민 <sml7179@naver.com>
2022-02-26 16:43:52 +0900

    feat: hello queryDSL!

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 실전 QueryDSL! 실습
 
 https://www.inflearn.com/course/Querydsl-%EC%8B%A4%EC%A0%84/dashboard
+
+QueryDsl 에서 컴파일 된 Q가 붙은 객체는 git에 올리지 않는게 좋다.
+시스템에서 만들어주기 때문에 로컬환경에 달라질 수 있기 때문이다.
--- src/test/java/study/querydsl/QuerydslStudyApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslStudyApplicationTests.java
@@ -1,13 +1,39 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class QuerydslStudyApplicationTests {
+
+    @Autowired //스프링에서 지원
+    // @PersistenceContext 자바 표준 스펙
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = jpaQueryFactory
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(hello).isEqualTo(result);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
