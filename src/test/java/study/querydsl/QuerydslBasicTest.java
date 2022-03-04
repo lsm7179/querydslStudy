76e5d33d57e8ed592aa9b8396be013259afa8e6d
이승민 <sml7179@naver.com>
2022-03-04 12:42:02 +0900

    세타 조인

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -236,4 +236,22 @@ public class QuerydslBasicTest {
                 .extracting("usertname")
                 .containsExactly("member1","member2");
     }
+
+    // 연관관계가 없이도 조인 할 수 있다. 세타 조인
+    @Test
+    void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
 }
