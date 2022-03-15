a8cca5c7cf14ae9f843964aa6eb610c56977c09c
이승민 <sml7179@naver.com>
2022-03-15 22:42:23 +0900

    스프링 데이터 페이징 활용3 - 컨트롤러 개발

--- src/main/java/study/querydsl/controller/MemberController.java
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -1,11 +1,14 @@
 package study.querydsl.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.repository.MemberJpaRepository;
+import study.querydsl.repository.MemberRepository;
 
 import java.util.List;
 
@@ -14,11 +17,23 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
-
+    private final MemberRepository memberRepository;
 
     // http://localhost:8080/v1/members?teamName=teamB&ageGoe=31&ageLoe=35&username=member31
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMember1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
+    }
+
+    //http://localhost:8080/v2/members?page=0&size=5
+    @GetMapping("v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    //http://localhost:8080/v3/members?page=0&size=200
+    @GetMapping("v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
